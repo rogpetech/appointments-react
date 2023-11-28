12dc20b46073b945e8f6ad52e9aaa2c1837db573
Rodolfo Peixoto <rodolfo.peixoto@rogpe.tech>
2023-11-28 16:04:32 -0300

    Add appoiments list with ol li

--- src/Appointment.js
+++ src/Appointment.js
@@ -3,3 +3,15 @@ import React from "react";
 export const Appointment = ({ customer }) => (
   <div>{customer.firstName}</div>
 );
+
+export const AppointmentsDayView = (
+  { appointments }
+ ) => (
+  <div id="appointmentsDayView">
+    <ol>
+      {appointments.map((appointment) => (
+        <li key={appointment.startsAt} />
+      ))}
+    </ol>
+  </div>
+)
--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { act } from "react-dom/test-utils"
-import { Appointment } from "../src/Appointment";
+import { Appointment, AppointmentsDayView } from "../src/Appointment";
 
 describe("Appointment", () => {
 
@@ -30,4 +30,42 @@ describe("Appointment", () => {
 
     expect(document.body.textContent).toContain("Jaquile");
   });
+});
+
+describe("AppointmentsDayView", () => {
+  let container;
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+
+  const render = (component) =>
+    act(()=> {
+      ReactDOM.createRoot(container).render(component);
+    });
+
+  it("renders a div with the right id", () => {
+    render(<AppointmentsDayView appointments={[]} />);
+
+    expect(document.querySelector("div#appointmentsDayView")).not.toBeNull();
+  });
+
+  it("renders an ol element to display appoiments", () => {
+    render(<AppointmentsDayView appointments={[]} />);
+
+    const listElement = document.querySelector("ol");
+    expect(listElement).not.toBeNull();
+  });
+
+  it("renders an li for each appoinments", () => {
+    const today = new Date();
+    const twoAppoiments = [
+      { startsAt: today.setHours(12, 0) },
+      { startsAt: today.setHours(13, 0) },
+    ];
+
+    render(<AppointmentsDayView appointments={twoAppoiments} />);
+    const listChildren = document.querySelectorAll("ol > li");
+    expect(listChildren).toHaveLength(2);
+  })
 });
